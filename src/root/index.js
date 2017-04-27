3a3e343a33b5e4197f4a21fdbd0c87b194006550
WeiHu <7430328@qq.com>
2017-04-27 13:33:24 +0800

    切换分支
    切换分支

--- src/root/index.js
+++ src/root/index.js
@@ -11,9 +11,19 @@ import {
   Navigator,
   Image,
   Button,
+  Platform,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
 import { StackNavigator , TabNavigator} from 'react-navigation';
+
+import {
+  createNavigator,
+  createNavigationContainer,
+  TabRouter,
+  addNavigationHelpers,
+} from 'react-navigation';
 
 import Popular from '../modules/popular'
 import PopularItem from '../modules/popularItem'
@@ -24,75 +34,59 @@ import Mine from '../modules/mine'
 import Setting from '../modules/setting'
 import Register from '../modules/register'
 
-const Popular_TabNavigatorConfig = {
-    animationEnabled: false, // 切换页面时是否有动画效果
-    tabBarPosition:   'bottom', // 显示在底端，android 默认是显示在页面顶端的
-    swipeEnabled:     true, // 是否可以左右滑动切换tab
-    tabBarPosition:   'top',
-    animationEnabled: true,
-    tabBarOptions:    {
-      activeTintColor:   'white',// 文字和图片选中颜色
-      inactiveTintColor: '#666666', // 文字和图片未选中颜色
-      showIcon:          true, // android 默认不显示 icon, 需要设置为 true 才会显示
-      showLabel:         true,
-      lazyLoad:          false,
-      tabBarPosition:    'top',
-      indicatorStyle:    {
-        height: 0  // 如TabBar下面显示有一条线，可以设高度为0后隐藏
-      }, 
-      tabStyle:{
-        backgroundColor: 'rgb(0,185,80)'
-      },
-      style: {
-         backgroundColor: 'rgb(0,185,80)', // TabBar 背景色
-      },
-      tabStyle:{
-        tintColor: 'red'
-      },
-      labelStyle: {
-         fontSize: 14, // 文字大小
-      },
-    },
-  
-};
-
-const Popular_RouteConfigs = 
-{
-    All: {
-      screen:            PopularItem,
-      navigationOptions: {
-        tabBar:    {
-        label:     'All',
-
-        },
-      },
-    },
-     Android: {
-      screen:            PopularItem,
-      navigationOptions: {
-        tabBar:    {
-        label:     'Android',
-
-        },
-      },
-    },
-     iOS: {
-      screen:            PopularItem,
-      navigationOptions: {
-        tabBar:    {
-        label:     'iOS',
-
-        },
-      },
-    },
+const CustomTabBar = ({ navigation }) => {
+  const { routes } = navigation.state;
+  return (
+    <View style={{ flexDirection: 'row', height: 30, backgroundColor: 'orange'}}>
+      {routes.map(route => (
+        <TouchableOpacity
+          onPress = {() => navigation.navigate(route.routeName)}
+          style   = {styles.tab}
+          key     = {route.routeName}
+        >
+          <Text>{route.routeName}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
 }
 
-const Popular_TabBars = TabNavigator(Popular_RouteConfigs,Popular_TabNavigatorConfig)
+const CustomTabView = ({ router, navigation }) => {
+  const { routes, index } = navigation.state;
+  const ActiveScreen = router.getComponentForState(navigation.state);
+  return (
+    <View style={styles.container}>
+      <CustomTabBar navigation={navigation} />
+      <ActiveScreen
+        navigation={addNavigationHelpers({
+          ...navigation,
+          state: routes[index],
+        })}
+      />
+    </View>
+  );
+};
+
+const CustomTabRouter = TabRouter({
+    All: {
+      screen:            PopularItem,
+    },
+    Android: {
+      screen:            PopularItem,
+    },
+    iOS: {
+      screen:            PopularItem,
+    },
+},{
+  initialRouteName: 'All',
+});
+
+const CustomTabs = createNavigationContainer(createNavigator(CustomTabRouter)(CustomTabView));
 
 const RouteConfigs = 
 {
     Popular: {
-      screen:            Popular_TabBars,
+      screen:            CustomTabs,
       navigationOptions: {
         tabBar:    {
         label:     'Popular',
@@ -200,3 +194,22 @@ export default class WeiBo extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    // marginTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    height: 30,
+  },
+  tab: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 4,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+  }
+});
